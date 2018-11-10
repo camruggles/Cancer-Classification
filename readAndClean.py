3a71f2f0b49f6d778ee146d933aec70bc01317ce
camruggles <cruggles@purdue.edu>
2018-11-10 11:46:32 -0500

    following flake8 conventions

--- readAndClean.py
+++ readAndClean.py
@@ -4,35 +4,32 @@ import numpy as np
 import pandas as pd
 
 
-#this uses all 32 columns, and gets the 'M' and 'B' labels and converts them to 1 and -1 for the y vector
-#I eliminated one column that had strictly nan values
+# this uses all 32 columns, and gets the 'M' and 'B' labels
+#   and converts them to 1 and -1 for the y vector
+# I eliminated one column that had strictly nan values
 def getCleanedData(filename):
-	data = pd.read_csv(filename)
+    data = pd.read_csv(filename)
 
-	n,d = data.shape
-	n = n
-	d = d-2
+    n, d = data.shape
+    n = n
+    d = d-2
 
-	v1 = data.values
-	v2 = v1[:, 1:32]
+    uncleaned = data.values
+    noNans = uncleaned[:, 1:32]
 
-	v3 = np.zeros((n,d))
-	v3[:, 1:] = v2[:, 1:]
-	for i in xrange(n):
-	    if v2[i, 0] == 'B':
-	        v3[i,0] = 1
-	    elif v2[i,0] == 'M':
-	        v3[i,0] = -1
-	    else:
-	        print 'Problem in cleaning data'
+    labelled = np.zeros((n, d))
+    labelled[:, 1:] = noNans[:, 1:]
+    for i in xrange(n):
+        if noNans[i, 0] == 'B':
+            labelled[i, 0] = 1
+        elif noNans[i, 0] == 'M':
+            labelled[i, 0] = -1
+        else:
+            print 'Problem in cleaning data'
 
-	y = v3[:, 0]
-	X = v3[:, 1:]
-	#print v3[0,:]
-	#print X[0,:]
-	#print y
-	return X,y
+    y = labelled[:, 0]
+    X = labelled[:, 1:]
+    return X, y
 
 
-
-X,y = getCleanedData("data.csv")
+X, y = getCleanedData("data.csv")
