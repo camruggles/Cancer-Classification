d4399782197b7c86cbfadf828772e5b08b2001e8
camruggles <cruggles@purdue.edu>
2018-11-10 11:31:39 -0500

    Getting and cleaning data

---
+++ run.py
@@ -0,0 +1,34 @@
+
+
+import numpy as np
+import pandas as pd
+def getCleanedData(filename):
+	data = pd.read_csv(filename)
+
+	n,d = data.shape
+	n = n
+	d = d-2
+
+	v1 = data.values
+	v2 = v1[:, 1:32]
+
+	v3 = np.zeros((n,d))
+	v3[:, 1:] = v2[:, 1:]
+	for i in xrange(n):
+	    if v2[i, 0] == 'B':
+	        v3[i,0] = 1
+	    elif v2[i,0] == 'M':
+	        v3[i,0] = -1
+	    else:
+	        print 'Problem in cleaning data'
+
+	y = v3[:, 0]
+	X = v3[:, 1:]
+	#print v3[0,:]
+	#print X[0,:]
+	#print y
+	return X,y
+
+
+
+X,y = getCleanedData("data.csv")
