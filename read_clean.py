371c982ee095e62884772d476211f20d3b797825
camruggles <cruggles@purdue.edu>
2018-11-12 17:31:27 -0500

    linting code

--- read_clean.py
+++ read_clean.py
@@ -1,23 +1,26 @@
 
 ####################################################
-# Cleaning Data because pandas doesnt work for me 
+# Cleaning Data because pandas doesnt work for me
 ####################################################
 
 import numpy as np
 import pandas as pd
 np.set_printoptions(suppress=True)
 
+
 def getCleanedData(filename):
     data = pd.read_csv(filename)
 
     # Diagnosis
-    y = data.iloc[:,1]
-    y = y.replace(['M','B'], [1, -1])
+    y = data.iloc[:, 1]
+    y = y.replace(['M', 'B'], [1, -1])
 
-    # Looking at columns: radius, texture, perimeter, area, smoothness, compactness, concavity, concave points
-    # symmetry and fractal dimension 
+    # Looking at columns: radius, texture, perimeter, area,
+    #   smoothness, compactness, concavity, concave points
+    # symmetry and fractal dimension
     data = data.iloc[:, 2:12]
-    
+
     return data.values, y.values
-    
-X, y = getCleanedData("data.csv")
+
+
+#  X, y = getCleanedData("data.csv")
