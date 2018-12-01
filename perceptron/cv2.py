96ffb5096f63dc01c0a3e345e9f07e104c2acfc6
camruggles <cruggles@purdue.edu>
2018-11-30 21:20:58 -0500

    Update cv2.py

--- perceptron/cv2.py
+++ perceptron/cv2.py
@@ -14,12 +14,6 @@ def cross_validation_split(y, folds=3):
     n_p = int(len(y_positive)/folds)
     n_n = int(len(y_negative)/folds)
 
-    print len(y_positive)
-    print len(y_negative)
-
-    print n_p
-    print n_n
-
     shuffle(y_negative)
     shuffle(y_positive)
 
@@ -28,20 +22,11 @@ def cross_validation_split(y, folds=3):
         split.append([])
     k = folds
     for i in range(0, k-1):
-        print 'range'
-        print (i)*n_p
-        print (i+1)*n_p
-        print ''
         split[i] += y_positive[i*n_p: (i+1)*n_p]
         split[i] += y_negative[i*n_n: (i+1)*n_n]
-    print 'range'
-    print (k-1)*n_p
-    print (k-1)*n_n
-    print ''
+
     split[k-1] += y_positive[(k-1)*n_p:]
     split[k-1] += y_negative[(k-1)*n_n:]
-
-    print split
 
     return split
 
@@ -63,9 +48,6 @@ def main():
 
     # extract 10 folds from the data
     split = cross_validation_split(y, foldcount)
-    # print(folds)
-    print n
-    print split
 
     # for each fold, figure out how many data points are in the folds
     #  excluding the one about to be tested
@@ -78,20 +60,12 @@ def main():
             trainInd += split[i]
 
         # construct the training set with the row count obtained
-        # and fill it with the training data
-        print 'testing fold: ', j
-        print 'train indices'
-        print trainInd
-        print 'test indices'
-        print testInd
+
         trainSet = X[trainInd]
         trainLabels = y[trainInd]
 
         testSet = X[testInd]
         testLabels = y[testInd]
-
-        print 'ylabels'
-        print testLabels
 
         theta = LP.train(1000, trainSet, trainLabels)
 
@@ -107,9 +81,6 @@ def main():
             test_label = testLabels[i]
             # count if the test was good or not
             testResult = LP.test(theta, test_point)
-            print 'labels'
-            print testResult
-            print test_label
 
             if testResult == 1 and test_label == 1:
                 tp += 1
@@ -121,8 +92,6 @@ def main():
                 tn += 1
 
         # print the results of the test
-        print 'tp, fp, fn, tn'
-        print tp, fp, fn, tn
 
         try:
             accuracy[j] = float(tp + tn) / float(fn + fp + tp + tn)
